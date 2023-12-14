e14d53dcd7ab22dc5dd5e3fa2b0216b90734775a
Đình Nam <namndwebdev@gmail.com>
2023-12-14 16:45:27 +0700

    Xoa version nhe nhang

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,30 @@ import './App.css'
 import { useFetch } from './common/customHook'
 import { Link } from 'react-router-dom'
 import {deleteTask} from './services/task'
-import { memo } from 'react'
-async function handleDelete(id, cb){
-  try {
-    await deleteTask(id)
-    cb()
-  } catch (error) {
-    console.log(error);
-    alert('khong the xoa')
-  }
-}
-const TaskElement = memo(function(props){
-  let item = props.item
-  console.log('render', item?.id);
-  return <Link  to={`/${item?.id}`}>
-  <h1>
-    {item?.id}
-    <button onClick={(e)=>{
-      e.preventDefault()
-      e.stopPropagation()
-      handleDelete(item?.id, props.afterDelete)
-      
-    }}>Xoa</button>
-  </h1>
-</Link>
-}, function(oldProps, newProps){
-  return oldProps.item.id == newProps.item.id
-})
-
 function App() {
-
   const [data, setData, paging, setPaging, reload, setReload] = useFetch('https://backoffice.nodemy.vn/api/tasks')
-  
+  async function handleDelete(id){
+    try {
+      await deleteTask(id)
+      setReload(!reload)
+    } catch (error) {
+      console.log(error);
+      alert('khong the xoa')
+    }
+  }
   return (
     <>
       {data?.map(item=>{
-        return <TaskElement 
-          key={item?.id} 
-          item={item}
-          afterDelete={()=>{
-            setReload(current=>{ return !current })
-          }}
-        />
+        return <Link key={item?.id} to={`/${item?.id}`}>
+          <h1>
+            {item?.id}
+            <button onClick={(e)=>{
+              e.preventDefault()
+              e.stopPropagation()
+              handleDelete(item?.id)
+            }}>Xoa</button>
+          </h1>
+        </Link>
       })}
 
       <button onClick={()=>{
